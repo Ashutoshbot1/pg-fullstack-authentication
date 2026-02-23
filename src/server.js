0b25a8e83d44e238ac9cd2c00940eaed92523342
Ashutoshbot1 <ashutoshbot121@gmail.com>
2026-02-23 16:35:34 +0530

    feat: Establish PostgreSQL database connection and integrate cookie-parser and JSON body parsing middlewares.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+// import pool from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5001;
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
